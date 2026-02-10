f384670551119290b7b2354d90952d8d1cc223fd
melselin <melisaselinmusellim@gmail.com>
2026-02-10 10:54:28 +0100

    update

--- Assets/Blabla.cs
+++ Assets/Blabla.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Input : MonoBehaviour
+public class Blabla : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
